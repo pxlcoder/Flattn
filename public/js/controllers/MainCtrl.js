3f794d7398d388cb4355a9b126267250295e3bda
Aditya Keerthi <pxlcoder@gmail.com>
2016-01-14 14:11:09 -0500

    Disabled create button while URL is being created

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,17 +1,23 @@
 angular.module('MainCtrl', []).controller('MainController', ['$scope', 'Link', function($scope, Link) {
 
-    $scope.placeholder = 'Flattn your URL!';   
+    $scope.placeholder = 'Flattn your URL!';
+    $scope.loading = false;   
 
     $scope.createURL = function(){
+        $scope.loading = true;
+
     	if ($scope.url){
 	    	Link.create($scope.url).success(function(data) {
 	    		$scope.url = data.id;
 	    		Materialize.toast("Done!", 2000, 'toast');
+                $scope.loading = false;
 	    	}).error(function (){
 	    		Materialize.toast("Failed", 2000, 'toast');
+                $scope.loading = false;
 	    	});
     	}else{
     		Materialize.toast("Failed", 2000, 'toast');
+            $scope.loading = false;
     	}
     };
 
